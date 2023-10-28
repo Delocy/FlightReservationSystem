b2fe6af829db886142f5436516401a314184fa99
Zares Tng <tngzhexuan@gmail.com>
2023-10-28 23:21:38 +0800

    added employee entity
    and employeeaccessrightenum

---
+++ FRS/FRSLibrary/src/entity/Employee.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import util.enumeration.EmployeeAccessRightEnum;
+
+/**
+ *
+ * @author zares
+ */
+@Entity
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long employeeID;
+    @Column(nullable = false, length = 32)
+    private String firstName;
+    @Column(nullable = false, length = 32)
+    private String lastName;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EmployeeAccessRightEnum accessRightEnum;
+    @Column(nullable = false, unique = true, length = 32)
+    private String username;
+    @Column(nullable = false, length = 32)
+    private String password;
+    
+    public Long getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(Long employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (employeeID != null ? employeeID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the employeeID fields are not set
+        if (!(object instanceof Employee)) {
+            return false;
+        }
+        Employee other = (Employee) object;
+        if ((this.employeeID == null && other.employeeID != null) || (this.employeeID != null && !this.employeeID.equals(other.employeeID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Employee[ id=" + employeeID + " ]";
+    }
+    
+}
